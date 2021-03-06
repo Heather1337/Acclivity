3167cf857e0442a66887b6338ccf7e8a1293c5ae
Heather1337 <heatherlynn1337@gmail.com>
2021-03-06 13:59:24 -0800

    Adding login react structure

---
+++ static/login.jsx
@@ -0,0 +1,58 @@
+function Login(props) {
+
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const history = useHistory();
+
+    function handleSubmit(evt) {
+        evt.preventDefault();
+        let data = {email:email, password:password};
+        fetch('/api/login',
+        {method: "POST",  body: JSON.stringify(data),  headers: {'Content-Type': 'application/json'}})
+        .then(response => response.json())
+        .then(data => {
+            if (data !== 'info does not match'){
+                props.setUser(data)
+                localStorage.setItem('user',JSON.stringify(data));
+                history.push('/homepage');
+            } else {
+                alert('Invalid Username or Password')
+            }
+        });
+    }
+
+    return (
+        <React.Fragment>
+
+            <Form id="loginform" onSubmit={handleSubmit}>
+
+            <Form.Group controlId="formBasicEmail">
+
+                <Form.Control type="email"
+                        name="login-email"
+                            placeholder="Enter email"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)} />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+
+                <Form.Control type="password"
+                                name="login-password"
+                                placeholder="Password"
+                                value={password}
+                                onChange={e => setPassword(e.target.value)}></Form.Control>
+                </Form.Group>
+
+            <Button id='login-button'
+                        variant="primary"
+                        type="submit">
+                        Login
+            </Button>
+
+            </Form>
+
+        </React.Fragment>
+    );
+}
+
