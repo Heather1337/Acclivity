0e7de53a7cc154b15208722ea6e13ef03e73391f
Angela Purcell <kalmpurcell@gmail.com>
2021-03-07 05:54:52 +0000

    added table to dashboard

--- static/scripts.js
+++ static/scripts.js
@@ -24,3 +24,4 @@ const DropdownType = ReactBootstrap.DropdownType;
 const Dropdown = ReactBootstrap.Dropdown;
 const DropdownButton = ReactBootstrap.DropdownButton;
 const NavDropdown = ReactBootstrap.NavDropdown;
+const Table = ReactBootstrap.Table
