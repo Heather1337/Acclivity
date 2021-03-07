baff116f8d24b4e8838d56d53c6979be68991d5c
Heather1337 <heatherlynn1337@gmail.com>
2021-03-07 11:11:27 -0800

    Adding route update

--- static/app.jsx
+++ static/app.jsx
@@ -18,7 +18,7 @@ function App() {
              {/* <TopNav user={user} setUser={setUser}/> */}
 
           <Switch>
-                  <Route exact path="/home">
+                  <Route exact path="/">
                       <TopNav user={user} setUser={setUser}/>
                       <Dashboard dashboardRefresher={dashboardRefresher}/>
                       <StocksContainer user={user} setUser={setUser} dashboardRefresher={dashboardRefresher} setDashboardRefresher={setDashboardRefresher}/>
