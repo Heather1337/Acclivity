78df7060702c44340c1b56da99a9d1d1b9dca541
Heather1337 <heatherlynn1337@gmail.com>
2021-03-06 19:22:35 -0800

    Adding seed function to update records

--- seed_db.py
+++ seed_db.py
@@ -25,22 +25,55 @@ stock_data = 'data/Acclivity.csv'
 
 
 # ************************************************************************************
+stocks = {}
+
 with open(stock_data, 'r') as file:
     csv_file = csv.DictReader(file)
     for row in csv_file:
-        stock = Stock(symbol=row['Ticker Symbol'].strip(),
-                      sector='fillinlater',
-                      # sector = row['Sector'].strip(), Might need to add another seed function for this if we need to use both csv's
-                      dividend_amount=row['Dividend Amount'].strip(),
-                      dividend_yield=row['Dividend Yield'].strip(),
-                      payout_schedule=row['Payout Schedule'].strip(),
-                      payout_ratio=row['Payout Ratio'].strip(),
-                      stock_price=5,
-                      date_added='11-04-2020',
-                      date_modified='11-04-2020')
+        symbol = row['Ticker Symbol'].strip()
+        if symbol not in stocks:
+            stocks[symbol] = {
+                "symbol": symbol,
+                "dividend_amount": row["Dividend Amount"].strip(),
+                "payout_schedule": row["Payout Schedule"].strip(),
+                "stock_price": row["Stock Price"].strip()
+            }
 
-        db.session.add(stock)
-        db.session.commit()
+with open('data/symbols.csv', 'r') as file:
+    csv_file = csv.DictReader(file)
+    for row in csv_file:
+        symbol = row['Ticker'].strip()
+        if symbol in stocks:
+            stocks[symbol]["company_name"] = row["Company Name"]
+            stocks[symbol]["sector"] = row["Sector"]
+            stocks[symbol]["payout_ratio"]: row["Dividend Payout Ratio"].strip()
+            if "Dividend Yield" in row:
+                stocks[symbol]["dividend_yield"]: row["Dividend Yield"].strip()
+            else:
+                stocks[symbol]["dividend_yield"] = 0
+
+for stock in stocks:
+    print('stock', stocks[stock])
+    ticker = Stock(symbol=stocks[stock]['symbol'], sector=stocks[stock]['sector'], company_name=stocks[stock]['company_name'], dividend_amount=stocks[stock]['dividend_amount'],
+                   dividend_yield=stocks[stock]['dividend_yield'], payout_schedule=stocks[
+        stock]['payout_schedule'], payout_ratio=stocks[stock]['payout_ratio'],
+        stock_price=stocks[stock]['stock_price'])
+
+    db.session.add(stock)
+    db.session.commit()
+    # stock = Stock(symbol=row['Ticker Symbol'].strip(),
+    #               sector='fillinlater',
+    #               # sector = row['Sector'].strip(), Might need to add another seed function for this if we need to use both csv's
+    #               dividend_amount=row['Dividend Amount'].strip(),
+    #               dividend_yield=row['Dividend Yield'].strip(),
+    #               payout_schedule=row['Payout Schedule'].strip(),
+    #               payout_ratio=row['Payout Ratio'].strip(),
+    #               stock_price=5,
+    #               date_added='11-04-2020',
+    #               date_modified='11-04-2020')
+
+    # db.session.add(stock)
+    # db.session.commit()
 
     # new_user = User(fname = 'User',
     #                 lname ='user',
